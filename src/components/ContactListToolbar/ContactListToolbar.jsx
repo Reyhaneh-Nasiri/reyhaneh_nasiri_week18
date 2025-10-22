844854df3c6e561bc8c400790ce96ec265cb20c3
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-23 01:17:12 +0330

    feat(contact-list-toolbar): add toolbar

---
+++ src/components/ContactListToolbar/ContactListToolbar.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import styles from "./ContactListToolbar.module.css";
+const ContactListToolbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className={styles.toolbar}>
+      <div
+        className={styles.itemsMenu}
+        style={{
+          height: `${isMenuOpen ? "fit-content" : "0"}`,
+          transform: `translateY(${isMenuOpen ? "0%" : "50%"})`,
+        }}
+      >
+        <p className={styles.itemMenu}>
+          <i class="fa-solid fa-plus"></i> New contact
+        </p>
+        <p className={styles.itemMenu}>
+          <i class="fa-solid fa-trash"></i> Delete
+        </p>
+      </div>
+      <div
+        className={styles.actionMenuButton}
+        onClick={() => setIsMenuOpen((isMenuOpen) => !isMenuOpen)}
+      >
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+      </div>
+    </div>
+  );
+};
+
+export default ContactListToolbar;
--- src/pages/ContactListPage/ContactListPage.jsx
+++ src/pages/ContactListPage/ContactListPage.jsx
@@ -1,8 +1,10 @@
 import SearchBox from "@/components/SearchBox/SearchBox";
 import styles from "./ContactListPage.module.css";
+import ContactListToolbar from "@/components/ContactListToolbar/ContactListToolbar";
 const ContactListPage = () => {
   return (
     <>
+      <ContactListToolbar />
       <SearchBox />
       <ul className={styles.contacts}>
         <li className={styles.contact}>
