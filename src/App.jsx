7588b55c448dc009f97d0efdd6902e778573feae
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-24 16:54:34 +0330

    feat(contacts): add state to manage contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react"
-import ContactListPage from "./pages/ContactListPage/ContactListPage"
-import AddContactPage from "./pages/AddContactPage/AddContactPage"
+import { useState } from "react";
+import ContactListPage from "./pages/ContactListPage/ContactListPage";
+import AddContactPage from "./pages/AddContactPage/AddContactPage";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("contact-list");
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "contact1",
+      email: "contact1@gmail.com",
+      phone: "1111",
+      job: "",
+    },
+        {
+      id: 2,
+      name: "contact2",
+      email: "contact2@gmail.com",
+      phone: "2222",
+      job: "",
+    },
+        {
+      id: 3,
+      name: "contact3",
+      email: "contact3@gmail.com",
+      phone: "3333",
+      job: "",
+    },
+  ]);
   return (
     <>
-      {currentPage === "contact-list" && <ContactListPage setCurrentPage={setCurrentPage} />}
-      {currentPage === "add-contact" && <AddContactPage setCurrentPage={setCurrentPage} />}
+      {currentPage === "contact-list" && (
+        <ContactListPage setCurrentPage={setCurrentPage} />
+      )}
+      {currentPage === "add-contact" && (
+        <AddContactPage setCurrentPage={setCurrentPage} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
