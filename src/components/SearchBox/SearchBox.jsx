b2c495972556b4867ab75a1767d8ce4cfd9ba3cb
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-25 03:45:08 +0330

    style(search-box): change style

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,14 +2,14 @@ import styles from "./SearchBox.module.css";
 const SearchBox = ({ setSearch }) => {
   return (
     <div className={styles.searchBox}>
+      <button>
+        <i className="fa-solid fa-search"></i>
+      </button>
       <input
         type="text"
         placeholder="Search contacts..."
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button>
-        <i className="fa-solid fa-search"></i>
-      </button>
     </div>
   );
 };
